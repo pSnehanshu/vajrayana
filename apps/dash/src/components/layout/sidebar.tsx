381c209c5dd1cc5c062170fbc9cdfd6f22f8d234
Snehanshu Phukon <hello@snehanshu.com>
2023-11-14 13:51:03 +0530

    make sidebar options work and custom icons

--- apps/dash/src/components/layout/sidebar.tsx
+++ apps/dash/src/components/layout/sidebar.tsx
@@ -1,7 +1,205 @@
-import { useRef } from "react";
+import { Link } from "react-router-dom";
 import classNames from "classnames";
 import { useOnClickOutside } from "usehooks-ts";
+import { ReactNode, useRef, useState } from "react";
+import { HiDocument } from "react-icons/hi2";
+import { MdElectricCar } from "react-icons/md";
+import { IoPieChartSharp } from "react-icons/io5";
+import { FaPlugCircleBolt, FaMoneyBillTrendUp } from "react-icons/fa6";
+import { FaChargingStation, FaBoxes, FaDotCircle } from "react-icons/fa";
 import { useStore } from "../../store";
+
+type MenuItem = {
+  title: string;
+  icon?: ReactNode;
+  link?: string;
+  children?: MenuItem[];
+};
+
+const menu: MenuItem[] = [
+  {
+    title: "Dashboard",
+    icon: <IoPieChartSharp />,
+    link: "/",
+  },
+  {
+    title: "Charging stations",
+    icon: <FaChargingStation />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Charging sessions",
+    icon: <FaPlugCircleBolt />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Customers",
+    icon: <MdElectricCar />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "External platforms",
+    icon: <FaBoxes />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Revenue",
+    icon: <FaMoneyBillTrendUp />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Reports",
+    icon: <HiDocument />,
+    children: [
+      {
+        title: "Settings",
+        link: "#",
+      },
+      {
+        title: "Kanban",
+        link: "#",
+      },
+      {
+        title: "Calendar",
+        link: "#",
+      },
+    ],
+  },
+];
+
+function MenuItem({ item }: { item: MenuItem }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return item.link ? (
+    <Link
+      to={item.link}
+      className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+    >
+      {item.icon ?? <FaDotCircle />}
+
+      <span className="ml-3">{item.title}</span>
+    </Link>
+  ) : (
+    <>
+      <button
+        onClick={() => setExpanded((v) => !v)}
+        type="button"
+        className={classNames(
+          expanded
+            ? "bg-gray-100 dark:bg-gray-700"
+            : "hover:bg-gray-100 dark:hover:bg-gray-700",
+          "flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group dark:text-white",
+        )}
+        aria-controls="dropdown-pages"
+      >
+        {item.icon ?? <FaDotCircle />}
+
+        <span className="flex-1 ml-3 text-left whitespace-nowrap">
+          {item.title}
+        </span>
+
+        {item.children && (
+          <svg
+            aria-hidden="true"
+            className={classNames(
+              { "rotate-180": expanded },
+              "w-6 h-6 transition-transform",
+            )}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+      </button>
+
+      {item.children && (
+        <ul className={classNames({ hidden: !expanded }, "py-2 space-y-2")}>
+          {item.children.map((citem, j) => (
+            <li key={j}>
+              <a
+                href={citem.link}
+                className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              >
+                {citem.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
 
 export function SideBar() {
   const mobileSideBarVisible = useStore((s) => s.mobileSideBarVisible);
@@ -49,239 +247,15 @@ export function SideBar() {
             />
           </div>
         </form>
+
         <ul className="space-y-2">
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-              </svg>
-              <span className="ml-3">Overview</span>
-            </a>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              aria-controls="dropdown-pages"
-              data-collapse-toggle="dropdown-pages"
-            >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                Pages
-              </span>
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            <ul id="dropdown-pages" className="hidden py-2 space-y-2">
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Settings
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Kanban
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Calendar
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              aria-controls="dropdown-sales"
-              data-collapse-toggle="dropdown-sales"
-            >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                Sales
-              </span>
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            <ul id="dropdown-sales" className="hidden py-2 space-y-2">
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Products
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Billing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Invoice
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" />
-                <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" />
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap">Messages</span>
-              <span className="inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
-                4
-              </span>
-            </a>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              aria-controls="dropdown-authentication"
-              data-collapse-toggle="dropdown-authentication"
-            >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                Authentication
-              </span>
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-            <ul id="dropdown-authentication" className="hidden py-2 space-y-2">
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Sign In
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Sign Up
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                >
-                  Forgot Password
-                </a>
-              </li>
-            </ul>
-          </li>
+          {menu.map((item, i) => (
+            <li key={i}>
+              <MenuItem item={item} />
+            </li>
+          ))}
         </ul>
+
         <ul className="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
           <li>
             <a
@@ -345,6 +319,7 @@ export function SideBar() {
           </li>
         </ul>
       </div>
+
       <div className="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white dark:bg-gray-800 z-20">
         <a
           href="#"
