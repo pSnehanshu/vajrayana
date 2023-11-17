ce36c6644bc72c838b4a82e3a305f23613be0d8c
Snehanshu Phukon <hello@snehanshu.com>
2023-11-17 13:09:37 +0530

    fix role selection validation

--- apps/dash/src/pages/settings/MemberSettings.tsx
+++ apps/dash/src/pages/settings/MemberSettings.tsx
@@ -195,6 +195,16 @@ type InviteMemberProps = {
   onOK: () => void;
 };
 
+const InvitememberSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  name: Yup.string(),
+  role: Yup.string().when("isOwner", {
+    is: false,
+    then: (s) => s.uuid().required(),
+  }),
+  isOwner: Yup.boolean().default(false),
+});
+
 function InviteMember({ onOK }: InviteMemberProps) {
   const rolesQuery = trpc.roles.list.useQuery({});
   const inviteMutation = trpc.members.invite.useMutation();
@@ -207,12 +217,7 @@ function InviteMember({ onOK }: InviteMemberProps) {
         role: "",
         isOwner: true,
       }}
-      validationSchema={Yup.object().shape({
-        email: Yup.string().email().required(),
-        name: Yup.string(),
-        role: Yup.string().uuid("Invalid role selected"),
-        isOwner: Yup.boolean().default(false),
-      })}
+      validationSchema={InvitememberSchema}
       onSubmit={async (values, { setSubmitting }) => {
         try {
           await inviteMutation.mutateAsync({
@@ -233,6 +238,8 @@ function InviteMember({ onOK }: InviteMemberProps) {
     >
       {(opts) => (
         <Form className="p-4">
+          {/* <pre>{JSON.stringify(opts, null, 2)}</pre> */}
+
           <div className="mb-4">
             <label
               htmlFor="name"
@@ -280,6 +287,7 @@ function InviteMember({ onOK }: InviteMemberProps) {
                 name="isOwner"
                 type="checkbox"
                 className="rounded-md w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onClick={() => opts.setTouched({ role: true })}
               />
             </div>
             <div className="ms-2 text-sm">
@@ -307,65 +315,61 @@ function InviteMember({ onOK }: InviteMemberProps) {
               </label>
 
               {rolesQuery.data ? (
-                <>
-                  <Listbox
-                    value={opts.values.role}
-                    onChange={(val) => {
-                      opts.setFieldValue("role", val);
-                    }}
-                  >
-                    <div className="relative mt-1">
-                      <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white dark:bg-gray-600 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                        <span className="block truncate">
-                          {rolesQuery.data.roles.get(opts.values.role)?.name ||
-                            "Select role"}
-                        </span>
+                <Listbox
+                  value={opts.values.role}
+                  onChange={(val) => opts.setFieldValue("role", val)}
+                >
+                  <div className="relative mt-1">
+                    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white dark:bg-gray-600 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                      <span className="block truncate">
+                        {rolesQuery.data.roles.get(opts.values.role)?.name ||
+                          "Select role"}
+                      </span>
 
-                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                          <PiCaretDownBold
-                            className="h-5 w-5 text-gray-400"
-                            aria-hidden="true"
-                          />
-                        </span>
-                      </Listbox.Button>
-                    </div>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <PiCaretDownBold
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+                  </div>
 
-                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 dark:bg-gray-600 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                      {Array.from(rolesQuery.data.roles).map(([, role]) => (
-                        <Listbox.Option
-                          key={role.id}
-                          value={role.id}
-                          className={({ active }) =>
-                            `relative cursor-default select-none py-2 pl-10 pr-4  ${
-                              active ? "dark:bg-gray-800" : ""
-                            }`
-                          }
-                        >
-                          {({ selected }) => (
-                            <>
-                              <span
-                                className={`block truncate ${
-                                  selected ? "font-medium" : "font-normal"
-                                }`}
-                              >
-                                {role.name}
+                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 dark:bg-gray-600 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                    {Array.from(rolesQuery.data.roles).map(([, role]) => (
+                      <Listbox.Option
+                        key={role.id}
+                        value={role.id}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4  ${
+                            active ? "dark:bg-gray-800" : ""
+                          }`
+                        }
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? "font-medium" : "font-normal"
+                              }`}
+                            >
+                              {role.name}
+                            </span>
+
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 dark:text-gray-400">
+                                <IoMdCheckmark
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
                               </span>
-
-                              {selected ? (
-                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 dark:text-gray-400">
-                                  <IoMdCheckmark
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Listbox>
-                </>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Listbox>
               ) : (
                 <p role="loading">Fetching roles...</p>
               )}
