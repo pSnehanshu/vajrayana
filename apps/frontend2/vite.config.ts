2ab8152e2c9386f0ff0c13cfe19b5d3807b1ffc4
Snehanshu Phukon <hello@snehanshu.com>
2024-02-24 13:14:31 +0530

    setup shadcn/ui

--- apps/frontend2/vite.config.ts
+++ apps/frontend2/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,11 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:22281/",
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
