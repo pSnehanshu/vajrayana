916196bc499475a1cd25151e55010f40edf19fa2
Snehanshu Phukon <hello@snehanshu.com>
2024-02-24 13:51:33 +0530

    setup tanstack router (file based routing)

---
+++ apps/frontend2/src/routes/__root.tsx
@@ -0,0 +1,32 @@
+import { lazy, Suspense } from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+
+const TanStackRouterDevtools = import.meta.env.DEV
+  ? lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    )
+  : () => null;
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
+    </>
+  ),
+});
