4bed08990e14ce2c2a5a7470dee03cdc7fc3b7c8
Snehanshu Phukon <hello@snehanshu.com>
2024-02-24 13:25:10 +0530

    delete old frontend

--- apps/frontend/src/components/full-page-loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ImSpinner2 } from "react-icons/im";
-
-export function FullPageLoading() {
-  return (
-    <div className="bg-gray-50 dark:bg-gray-900 dark:text-white h-screen w-screen flex justify-center items-center">
-      <ImSpinner2 className="text-8xl animate-spin" />
-    </div>
-  );
-}
