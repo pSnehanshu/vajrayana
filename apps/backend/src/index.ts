0ec857bbf6f73b989bff3b400c45a187b599b61c
Snehanshu Phukon <hello@snehanshu.com>
2024-02-26 14:04:59 +0530

    add /api/check

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,7 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./routers";
 import { createContext } from "./trpc";
 import { CreateWebsocketServer } from "./websocket";
+import { prisma } from "@zigbolt/prisma";
 
 const app = express();
 const port = parseInt(process.env.PORT ?? "22281", 10);
@@ -19,6 +20,15 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 const api = express.Router();
 
 app.use("/api", api);
+
+// Here we can check status of different services this app depends on
+api.get("/check", async (req, res) => {
+  // Check DB session TZ
+  const tz =
+    await prisma.$queryRaw`SELECT current_setting('TIMEZONE') AS session_timezone_offset;`;
+
+  res.json(tz);
+});
 
 api.use(
   "/trpc",
