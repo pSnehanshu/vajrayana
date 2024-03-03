235d639052230b5faea9159542133c402a2a90da
Snehanshu Phukon <hello@snehanshu.com>
2024-03-03 22:00:07 +0530

    improve CS table and WS connect logic

--- apps/admin-dash/src/routes/charging-stations/index.tsx
+++ apps/admin-dash/src/routes/charging-stations/index.tsx
@@ -36,29 +36,46 @@ const columns = [
   columnHelper.accessor("friendlyName", {
     header: "Name",
     cell: (props) => (
-      <div>
-        <Link
-          to={`/charging-stations/$stationId`}
-          params={{ stationId: props.row.original.id }}
-          className="text-lg hover:underline"
-        >
-          {props.getValue()}
-        </Link>
-
-        <div className="my-1">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <span
-                onClick={() => copy(props.row.original.id)}
-                className="text-xs py-0.5 px-1 cursor-pointer font-mono rounded-md text-secondary-foreground bg-secondary"
-              >
-                ID: {props.row.original.id}
-              </span>
-            </TooltipTrigger>
-            <TooltipContent side="right">Click to copy</TooltipContent>
-          </Tooltip>
-        </div>
-      </div>
+      <Link
+        to={`/charging-stations/$stationId`}
+        params={{ stationId: props.row.original.id }}
+        className="text-lg hover:underline"
+      >
+        {props.getValue()}
+      </Link>
+    ),
+  }),
+  columnHelper.accessor("id", {
+    header: "ID",
+    cell: (props) => (
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span
+            onClick={() => copy(props.row.original.id)}
+            className="text-xs py-0.5 px-1 cursor-pointer font-mono rounded-md text-secondary-foreground bg-secondary"
+          >
+            {props.row.original.id}
+          </span>
+        </TooltipTrigger>
+        <TooltipContent side="right">Click to copy</TooltipContent>
+      </Tooltip>
+    ),
+  }),
+  columnHelper.accessor("urlName", {
+    header: "URL Name",
+    cell: (props) => (
+      <span className="font-mono">/api/ocpp/{props.getValue()}</span>
+    ),
+  }),
+  columnHelper.display({
+    id: "hardware",
+    header: "Hardware info",
+    cell: (props) => (
+      <>
+        <p>Vendor: {props.row.original.vendorName ?? "N/A"}</p>
+        <p>Model: {props.row.original.model ?? "N/A"}</p>
+        <p>S/N: {props.row.original.serialNumber ?? "N/A"}</p>
+      </>
     ),
   }),
 ];
@@ -72,47 +89,49 @@ function ChargingStationsList() {
   });
 
   return (
-    <Table>
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <TableHead key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                </TableHead>
-              );
-            })}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody>
-        {table.getRowModel().rows?.length ? (
-          table.getRowModel().rows.map((row) => (
-            <TableRow
-              key={row.id}
-              data-state={row.getIsSelected() && "selected"}
-            >
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
+    <>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
             </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={columns.length} className="h-24 text-center">
-              No results.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
   );
 }
--- apps/backend/src/websocket/index.ts
+++ apps/backend/src/websocket/index.ts
@@ -8,65 +8,54 @@ export function CreateWebsocketServer(server: Server) {
   server.on("upgrade", async (request, socket, head) => {
     socket.on("error", console.error);
 
-    const { url, headers } = request;
-
-    const hostName = headers.host?.split(":").at(0)?.trim();
-
-    if (!hostName) {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      socket.destroy(
-        new Error(`host name cannot be determined, received: ${headers.host}`),
-      );
-      return;
-    }
-
-    if (!url) {
+    if (!request.url) {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       socket.destroy(new Error("url not specified"));
       return;
     }
 
     // Check path
-    if (url.startsWith("/api/ocpp/")) {
-      /** The Charging station URL name */
-      const urlName = url.split("/").at(3);
-
-      if (!urlName) {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        socket.destroy(new Error("charging station name not specified"));
-        return;
-      }
-
-      const chargingStation = await prisma.chargingStation.findUnique({
-        where: { urlName },
-      });
-
-      if (!chargingStation) {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        socket.destroy(new Error("charging station not found"));
-        return;
-      }
-
-      // Upgrade the connection to a WebSocket connection
-      ocppWSS.handleUpgrade(request, socket, head, (ws) => {
-        // Ensure only supported protocol
-        if (ws.protocol !== "ocpp2.0.1") {
-          ws.close(1002, `Recevied protocol: ${ws.protocol}`);
-          return;
-        }
-
-        // Set connection data
-        ConnectionsMap.set(ws, {
-          urlName,
-          chargingStation,
-          version: ws.protocol,
-        });
-
-        // Emit connection
-        ocppWSS.emit("connection", ws, request);
-      });
-    } else {
-      socket.destroy(new Error(`Not found. URL: ${url}`));
+    if (!request.url.startsWith("/api/ocpp/")) {
+      socket.destroy(new Error(`Not found. URL: ${request.url}`));
+      return;
     }
+
+    /** The Charging station URL name */
+    const urlName = request.url.split("/").at(3);
+
+    if (!urlName) {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.destroy(new Error("charging station name not specified"));
+      return;
+    }
+
+    const chargingStation = await prisma.chargingStation.findUnique({
+      where: { urlName },
+    });
+
+    if (!chargingStation) {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.destroy(new Error("charging station not found"));
+      return;
+    }
+
+    // Upgrade the connection to a WebSocket connection
+    ocppWSS.handleUpgrade(request, socket, head, (ws) => {
+      // Ensure only supported protocol
+      if (ws.protocol !== "ocpp2.0.1") {
+        ws.close(1002, `Recevied protocol: ${ws.protocol}`);
+        return;
+      }
+
+      // Set connection data
+      ConnectionsMap.set(ws, {
+        urlName,
+        chargingStation,
+        version: ws.protocol,
+      });
+
+      // Emit connection
+      ocppWSS.emit("connection", ws, request);
+    });
   });
 }
